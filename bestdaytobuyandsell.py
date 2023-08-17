03150d02ea6937d0d4578e5ef1117a2f46251100
Piotr98765 <61555492+PiotrCheski@users.noreply.github.com>
2023-08-17 17:04:23 +0200

    Create bestdaytobuyandsell.py

---
+++ bestdaytobuyandsell.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left_pointer, right_pointer = 0, 1
+        max_profit = 0
+        while right_pointer < len(prices):
+            if prices[left_pointer] < prices[right_pointer]:
+                curr_profit = prices[right_pointer] - prices[left_pointer]
+                max_profit = max(max_profit, curr_profit)
+            else:
+                left_pointer = right_pointer
+            right_pointer += 1
+        return max_profit
+    
+data = [2, 1, 0]
+solution_test = Solution().maxProfit(data)
+print(solution_test)
