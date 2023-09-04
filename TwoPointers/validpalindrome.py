78debd0ba5ce88e3a84c621c4ac5ea55ce29858f
Piotr98765 <61555492+PiotrCheski@users.noreply.github.com>
2023-09-04 19:01:32 +0200

    Update validpalindrome.py

--- TwoPointers/validpalindrome.py
+++ TwoPointers/validpalindrome.py
@@ -1,7 +1,24 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        cleaned = [char.lower() for char in s if char.isalnum()]
-        return cleaned == cleaned[::-1]
+        l = 0
+        r = len(s) - 1
+        
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+    def alphaNum(self, c):
+        if (ord("A") <= ord(c) <= ord("Z") or
+            ord("a") <= ord(c) <= ord("z") or
+            ord("0") <= ord(c) <= ord("9")):
+            return True
 
 t = "Race, a car"
 print(Solution().isPalindrome(t))
