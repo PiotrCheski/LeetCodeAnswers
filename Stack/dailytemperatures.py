3674cc98040e1bfefaa7bd0fc6b4055cd563b92f
Piotr98765 <61555492+PiotrCheski@users.noreply.github.com>
2023-09-10 18:10:59 +0200

    Update

--- Stack/dailytemperatures.py
+++ Stack/dailytemperatures.py
@@ -3,13 +3,15 @@ from typing import List
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0] * len(temperatures)
-        stack = [] # pair: [temp, index]
+        stack = [] #pair: [temp, index]
 
         for i, t in enumerate(temperatures):
             while stack and t > stack[-1][0]:
-                stackT, stackInd = stack.pop()            
-                res[stackInd] = (i - stackInd)
+                stackT, stackInd = stack.pop()
+                res[stackInd] = (i-stackInd)
             stack.append([t, i])
         return res
-temperatures = [73,74,75,71,69,72,76,73]
+
+
+temperatures = [73,74]
 print(Solution().dailyTemperatures(temperatures))
