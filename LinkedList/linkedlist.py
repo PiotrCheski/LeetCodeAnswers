bb18608453334fa116144b9bdb3504410edf671e
Piotr98765 <61555492+PiotrCheski@users.noreply.github.com>
2023-09-28 21:50:30 +0200

    Update

--- LinkedList/linkedlist.py
+++ LinkedList/linkedlist.py
@@ -9,11 +9,23 @@ def printLinkedList(head):
         print(curr.val)
         curr = curr.nxt
 
+def reverseLinkedList(head):
+    prev = None
+    curr = head
+    while curr:
+        temp_nxt = curr.nxt
+        curr.nxt = prev
+        prev = curr
+        curr = temp_nxt
+    return prev
+
 a1 = linkedList(1)
 a2 = linkedList(2)
 a3 = linkedList(3)
 
 a1.nxt = a2
 a2.nxt = a3
+
+a1 = reverseLinkedList(a1)
 
 printLinkedList(a1)
