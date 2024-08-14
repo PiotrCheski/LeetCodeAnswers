ae48a653cdea19f31bd80aca5c5e606cfddcc7d4
Piotr98765 <61555492+PiotrCheski@users.noreply.github.com>
2024-08-14 19:27:01 +0200

    Create movezeros.py

---
+++ ArraysHashing/movezeros.py
@@ -0,0 +1,10 @@
+def moveZeros(nums):
+    l = 0
+    for r in range(len(nums)):
+        if nums[r] != 0:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+    return nums
+ 
+nums_t = [0,1,0]
+print(moveZeros(nums_t))
