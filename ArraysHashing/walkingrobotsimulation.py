b93f52de0e81cf4e717eecf8429eaed8e987ba33
Piotr98765 <61555492+PiotrCheski@users.noreply.github.com>
2024-09-05 17:51:52 +0200

    Update walkingrobotsimulation.py

--- ArraysHashing/walkingrobotsimulation.py
+++ ArraysHashing/walkingrobotsimulation.py
@@ -1,36 +1,25 @@
 def robotSim(commands, obstacles):
-    POS_X = 0
-    POS_Y = 0
-    MODE = 0
-    output = 0
-    for idx, number in enumerate(commands):
-        if number >= 1 and number <= 9:
-            if MODE == 0:
-                POS_Y += number
-                possible_next_location = [POS_X, POS_Y]
-                if possible_next_location in obstacles:
-                    POS_Y -= number
+    pos_x = 0
+    pos_y = 0
+    mode = 0
+    DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    obstacles_set = set(map(tuple, obstacles))
+    max_distance = 0
+    for command in commands:
+        if command == -2:
+            mode = (mode - 1) % 4
+        elif command == -1:
+            mode = (mode + 1) % 4
+        else:
+            dx, dy = DIRECTIONS[mode]
+            for _ in range(command):
+                next_pos_x, next_pos_y = pos_x + dx, pos_y + dy
+                if (next_pos_x, next_pos_y) not in obstacles_set:
+                    pos_x, pos_y = next_pos_x, next_pos_y
+                    max_distance = max(max_distance, pos_x **2 + pos_y**2)
+                else:
                     break
-            elif MODE == -1:
-                POS_X += number
-                possible_next_location = [POS_X, POS_Y]
-                if possible_next_location in obstacles:
-                    POS_X -= number
-                    break
-            elif MODE == -2:
-                POS_X -= number
-                possible_next_location = [POS_X, POS_Y]
-                if possible_next_location in obstacles:
-                    POS_X += number
-                    break
-        elif number == -1:
-            MODE = -1
-        elif number == -2:
-            MODE =- 2
-        print(POS_X, POS_Y)
-        euclidian_distance = POS_X**2 + POS_Y**2
-        output = max(euclidian_distance, output)
-    return output
+    return max_distance
 
 commands_t = [6,-1,-1,6]
 obstacles_t = []
